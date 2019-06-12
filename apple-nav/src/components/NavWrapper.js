4e7f385fa9dd40f91469945de59fab6b85ce0693
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-12 07:54:52 +0100

    Created  header element and inported Nav Component

--- apple-nav/src/components/NavWrapper.js
+++ apple-nav/src/components/NavWrapper.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import navData from '../navData';
+import Nav from './Nav';
 
 export default () => (
-  <div>
-    {navData.map(data => <NavLink key={data.id} to='#' >{data.title}</NavLink>)}
-  </div>
+  <header>
+    <Nav />
+  </header>
 );
